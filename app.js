ba31e64ae6cc1eaf159d1f279759231497ea5994
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-16 22:22:54 +0530

    add style

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const add = (a,b) => +a + +b
 const subtract = (a,b) => +a - +b
 const mulitply = (a,b) => +a * +b
 const divide = (a,b) => +a / +b
-let firstNumber
-let operation
-let secondNumber
+
+
 const operate = (a,b,c) =>{
     if(c==='+') return add(a,b)
     else if(c==='-') return subtract(a,b)
@@ -12,15 +11,20 @@ const operate = (a,b,c) =>{
     else return mulitply(a,b)
 }
 
-
 const digits = document.querySelectorAll('.digits')
 const operators = document.querySelectorAll('.operator')
 const equalTo = document.querySelector('.equalTo')
 const clear = document.querySelector('.clear')
-const display = document.querySelector('.display')
+const exp = document.querySelector('.expression')
+
+let firstNumber
+let operation
+let secondNumber
+let displayValue
 
 function populate(e){
-    display.innerHTML += this.value;
+    exp.innerHTML += this.value;
+    
 }
 digits.forEach(digit=>{
     digit.addEventListener('click',populate)
@@ -28,30 +32,33 @@ digits.forEach(digit=>{
 operators.forEach(operator=>{
     operator.addEventListener('click', populate)
 })
-clear.addEventListener('click', ()=>{
-    display.innerHTML = ''
-})
 equalTo.addEventListener('click',evaluate)
-function evaluate(){
-    const str = display.innerHTML
-    let a
-    let b
-    let numbers = str.split(/[+*-/]/)
-    let operations = str.split(/[0-9]/).filter(e=>e!=='')
-    
-    console.log(numbers)
-    console.log(operations)
-    // const arr = str.split(' ')
-    for(let i in operations){
-        a = numbers[0]
-        b = numbers[1]
-        console.log(a)
-        console.log(b)
-        console.log(operations[i])
-        console.log(operate(a,b,operations[i]))
-        numbers.splice(0,2,operate(a,b,operations[i]))
-    }
-    display.innerHTML = numbers[0]
-}
+clear.addEventListener('click', ()=>{
+    exp.innerHTML = ''
+})
+
+
+
+
+
+
+
+
+// function evaluate(){
+
+//     const str = display.innerHTML
+//     let a
+//     let b
+//     let numbers = str.split(/[+*/-]/)
+//     let operations = str.split(/[0-9]/).filter(e=>e!==''&&e!=='.')
+
+//     for(let i in operations){
+//         a = numbers[0]
+//         b = numbers[1]
+//         numbers.splice(0,2,operate(a,b,operations[i]))
+//     }
+//     display.innerHTML = numbers[0]
+
+// }
 
 
