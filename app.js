e8d1ec0f45708d383dd2ed7a4193ff01823e7d4a
bharath197 <125417055+bharath197@users.noreply.github.com>
2023-04-17 04:16:12 +0530

    change functionality

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const add = (a,b) => +a + +b
-const subtract = (a,b) => +a - +b
-const mulitply = (a,b) => +a * +b
-const divide = (a,b) => +a / +b
+const add = (a, b) => +a + +b
+const subtract = (a, b) => +a - +b
+const mulitply = (a, b) => +a * +b
+const divide = (a, b) => +a / +b
 
 
-const operate = (a,b,c) =>{
-    if(c==='+') return add(a,b)
-    else if(c==='-') return subtract(a,b)
-    else if(c==='/') return divide(a,b)
-    else return mulitply(a,b)
+const operate = (a, b, c) => {
+    if (c === '+') return add(a, b)
+    else if (c === '-') return subtract(a, b)
+    else if (c === '/') return divide(a, b)
+    else return mulitply(a, b)
 }
 
 const digits = document.querySelectorAll('.digits')
@@ -16,49 +16,65 @@ const operators = document.querySelectorAll('.operator')
 const equalTo = document.querySelector('.equalTo')
 const clear = document.querySelector('.clear')
 const exp = document.querySelector('.expression')
+const res = document.querySelector('.result')
 
-let firstNumber
-let operation
-let secondNumber
-let displayValue
+let firstNumber = ''
+let operation = ''
+let secondNumber = ''
 
-function populate(e){
-    exp.innerHTML += this.value;
-    
-}
-digits.forEach(digit=>{
-    digit.addEventListener('click',populate)
+
+
+
+
+digits.forEach(digit => digit.addEventListener('click', () => {
+    if (!operation) {
+        firstNumber += digit.value;
+        exp.textContent += digit.value
+    }
+    else {
+        secondNumber += digit.value
+        exp.textContent += digit.value
+        res.textContent = operate(firstNumber, secondNumber, operation)
+    }
+}))
+
+
+operators.forEach(operator => operator.addEventListener('click', () => {
+    if (!firstNumber) return
+    if (!secondNumber) {
+        operation = operator.value
+        exp.textContent = firstNumber + operator.value
+    }
+    if (secondNumber) {
+        firstNumber = res.textContent
+        operation = operator.value
+        secondNumber = ''
+        exp.textContent = res.textContent + operator.value
+        res.textContent = ''
+    }
+}))
+
+clear.addEventListener('click', () => {
+    firstNumber = ''
+    operation = ''
+    secondNumber = ''
+    res.textContent = ''
+    exp.textContent = ''
 })
-operators.forEach(operator=>{
-    operator.addEventListener('click', populate)
+
+equalTo.addEventListener('click', ()=>{
+    if(firstNumber && operation && secondNumber){
+        exp.textContent = res.textContent
+        firstNumber = res.textContent
+        operation = ''
+        secondNumber = ''
+        res.textContent = ''
+    }
+    else if(firstNumber && operation && !secondNumber){
+        alert('invalid')
+    }
+    else{
+        return
+    }
 })
-equalTo.addEventListener('click',evaluate)
-clear.addEventListener('click', ()=>{
-    exp.innerHTML = ''
-})
-
-
-
-
-
-
-
-
-// function evaluate(){
-
-//     const str = display.innerHTML
-//     let a
-//     let b
-//     let numbers = str.split(/[+*/-]/)
-//     let operations = str.split(/[0-9]/).filter(e=>e!==''&&e!=='.')
-
-//     for(let i in operations){
-//         a = numbers[0]
-//         b = numbers[1]
-//         numbers.splice(0,2,operate(a,b,operations[i]))
-//     }
-//     display.innerHTML = numbers[0]
-
-// }
-
 
